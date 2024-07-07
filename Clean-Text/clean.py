71fdb1cd13b065eedcf93dadf263653fa58db2b9
Pyrokryos <dulina.perera@outlook.com>
2024-07-07 11:38:09 +0530

    Clean-Text: The text cleaning library `clean-text` is imperfect. Have to implement that functionality on my own.

---
+++ Clean-Text/clean.py
@@ -0,0 +1,17 @@
+from cleantext import clean
+
+with open("input.txt", "r") as file:
+    text: str = file.read()
+
+cleaned_text: str = clean(
+    text,
+    fix_unicode=True,
+    to_ascii=True,
+    no_urls=True,
+    no_emails=True,
+    no_phone_numbers=True,
+    no_currency_symbols=True
+)
+
+with open("output.txt", "w") as file:
+    file.write(cleaned_text)
