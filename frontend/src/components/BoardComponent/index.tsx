320e19b734990cce7be52ba1da9af2cf67cac164
Dulina-Perera <dulina.perera@outlook.com>
2024-08-20 21:37:36 +0530

    Designed two buttons. One to add a node and the other to delete a node.

--- frontend/src/components/BoardComponent/index.tsx
+++ frontend/src/components/BoardComponent/index.tsx
@@ -1,50 +1,50 @@
+import ButtonComponent from '../ButtonComponent';
 import styles from './styles.module.css';
 import { Component, createSignal, onMount } from 'solid-js';
 
 const BoardComponent: Component = () => {
+	/* Signals */
 	const [clicked_position, set_clicked_position] = createSignal<{ x: number, y: number }>({ x: -1, y: -1 });
 	const [grabbing_board, set_grabbing_board] = createSignal<boolean>(false);
 	const [scale, set_scale] = createSignal<number>(1);
+	const [selected_node, set_selected_node] = createSignal<string | null>(null);
 
 	onMount(() => {
-		const board_element = document.getElementById('board');
+		const board_element: HTMLElement | null = document.getElementById('board');
 
 		if (board_element) {
-			board_element.addEventListener('wheel', (event: WheelEvent) => {
-				// Set the scale of the board.
-				set_scale(scale() + event.deltaY * -0.005);
+			board_element.addEventListener(
+				'wheel',
+				(event: WheelEvent) => {
+					set_scale(scale() + event.deltaY * -0.005); // Set the scale of the board.
+					set_scale(Math.min(Math.max(1, scale()), 2)); // Restrict the scale of the board.
 
-				// Restrict the scale of the board.
-				set_scale(Math.min(Math.max(1, scale()), 2));
-
-				// Apply the scale to the board.
-				board_element.style.transform = `scale(${scale()})`;
-				board_element.style.marginTop = `${(scale() - 1) * 50}vh`
-				board_element.style.marginLeft = `${(scale() - 1) * 50}vw`
-			});
+					// Apply the scale to the board.
+					board_element.style.transform = `scale(${scale()})`;
+					board_element.style.marginTop = `${(scale() - 1) * 50}vh`;
+					board_element.style.marginLeft = `${(scale() - 1) * 50}vw`;
+				},
+				{ passive: false }
+			);
 		}
 	});
 
-	const handleOnMouseDownBoard = (event: MouseEvent) => {
-		// Start grabbing the board.
+	const handle_on_mouse_down_board = (event: MouseEvent) => {
 		set_grabbing_board(true);
 		set_clicked_position({ x: event.x, y: event.y });
 	};
 
-	const handleOnMouseUpBoard = (event: MouseEvent) => {
+	const handle_on_mouse_up_board = (event: MouseEvent) => {
 		set_clicked_position({ x: -1, y: -1 });
-
-		// Stop grabbing the board.
 		set_grabbing_board(false);
 	};
 
-	const handleOnMouseMoveBoard = (event: MouseEvent) => {
-		// User clicked on the board.
+	const handle_on_mouse_move_board = (event: MouseEvent) => {
 		if (clicked_position().x >= 0 && clicked_position().y >= 0) {
-			const delta_x = event.x - clicked_position().x;
-			const delta_y = event.y - clicked_position().y;
+			const delta_x: number = event.x - clicked_position().x;
+			const delta_y: number = event.y - clicked_position().y;
 
-			const board_wrapper_element = document.getElementById('board_wrapper');
+			const board_wrapper_element: HTMLElement | null = document.getElementById('board_wrapper');
 			if (board_wrapper_element) {
 				board_wrapper_element.scrollBy(-delta_x, -delta_y);
 				set_clicked_position({ x: event.x, y: event.y });
@@ -52,14 +52,23 @@ const BoardComponent: Component = () => {
 		}
 	};
 
+	const handle_on_click_add = (number_inputs: number, number_outputs: number) => {
+
+	};
+
+	const handle_on_click_delete = () => {
+
+	};
+
 	return (
-		<div class={styles.wrapper} id="board_wrapper">
+		<div class={styles.wrapper} id='board_wrapper'>
+			<ButtonComponent show_delete={selected_node() !== null} on_click_add={handle_on_click_add} on_click_delete={handle_on_click_delete}></ButtonComponent>
 			<div
-				class={grabbing_board() ? styles.board_dragging : styles.board }
-				id="board"
-				onMouseDown={handleOnMouseDownBoard}
-				onMouseUp={handleOnMouseUpBoard}
-				onMouseMove={handleOnMouseMoveBoard}
+				class={grabbing_board() ? styles.board_dragging : styles.board}
+				id='board'
+				onMouseDown={handle_on_mouse_down_board}
+				onMouseUp={handle_on_mouse_up_board}
+				onMouseMove={handle_on_mouse_move_board}
 			></div>
 		</div>
 	);
--- frontend/src/components/ButtonComponent/index.tsx
+++ frontend/src/components/ButtonComponent/index.tsx
@@ -3,7 +3,7 @@ import { Component } from "solid-js";
 
 interface ButtonProps {
 	show_delete: boolean;
-	on_click_add: (number_inputs: number, number_outputs: number) => void;
+	on_click_add: (input_cnt: number, output_cnt: number) => void;
 	on_click_delete: () => void;
 }
 
@@ -11,10 +11,30 @@ const ButtonComponent: Component<ButtonProps> = (props: ButtonProps) => {
 	return (
 		<div class={styles.wrapper}>
 			<button class={props.show_delete ? styles.button_delete : styles.button_delete_hidden} onClick={props.on_click_delete}>
-				<svg fill="currentColor" stroke-width="0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" height="1em" width="1em" style="overflow: visible; color: currentcolor;"><path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0h120.4c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64s14.3-32 32-32h96l7.2-14.3zM32 128h384v320c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"></path></svg>
+			<svg
+				fill="currentColor"
+				stroke-width="0"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 448 512"
+				height="1em"
+				width="1em"
+				style="overflow: visible; color: currentcolor;"
+			>
+				<path d="M135.2 17.7C140.6 6.8 151.7 0 163.8 0h120.4c12.1 0 23.2 6.8 28.6 17.7L320 32h96c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 96 0 81.7 0 64s14.3-32 32-32h96l7.2-14.3zM32 128h384v320c0 35.3-28.7 64-64 64H96c-35.3 0-64-28.7-64-64V128zm96 64c-8.8 0-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16zm96 0c-8.8 0-16 7.2-16 16v224c0 8.8 7.2 16 16 16s16-7.2 16-16V208c0-8.8-7.2-16-16-16z"></path>
+			</svg>
 			</button>
 			<button class={styles.button_add}>
-				<svg fill="currentColor" stroke-width="0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" height="1em" width="1em" style="overflow: visible; color: currentcolor;"><path d="M14 7v1H8v6H7V8H1V7h6V1h1v6h6z"></path></svg>
+				<svg
+					fill="currentColor"
+					stroke-width="0"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 16 16"
+					height="1em"
+					width="1em"
+					style="overflow: visible; color: currentcolor;"
+				>
+					<path d="M14 7v1H8v6H7V8H1V7h6V1h1v6h6z"></path>
+				</svg>
 			</button>
 		</div>
 	);
