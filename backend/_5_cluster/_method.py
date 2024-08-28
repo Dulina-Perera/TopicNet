74d8e5c1c15f1737f750112665272f7c4bcfff35
Dulina-Perera <dulina.perera@outlook.com>
2024-08-28 08:59:08 +0530

    Included the `PDF-Extract-Kit` library.

--- backend/_5_cluster/_method.py
+++ backend/_5_cluster/_method.py
@@ -1,7 +1,8 @@
+from backend._5_cluster._clusterer import BaseClusterer
 from numpy import ndarray
 
 def cluster_embeddings(
-	clusterer,
+	clusterer: BaseClusterer,
 	embeddings: ndarray
 ) -> ndarray:
 	pass
