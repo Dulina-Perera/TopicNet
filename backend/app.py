3c487a9618693a7a848ae414c3d0e83c3a790782
Dulina-Perera <dulina.perera@outlook.com>
2024-09-12 23:25:39 +0530

    Defined logging configuration in a `.yaml` file.

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,34 @@
+# backend/app.py
+
+# %%
+import warnings; warnings.filterwarnings('ignore')
+import os, sys; sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import logging
+import logging.config
+import yaml
+
+from dotenv import load_dotenv
 from robyn import Robyn
+from typing import Any, Dict
 
-app = Robyn(__file__)
+# %%
+load_dotenv()
+
+with open('log_config.yaml', 'r') as f:
+	log_config: Dict[str, Any] = yaml.safe_load(f.read())
+
+logging.config.dictConfig(log_config)
+
+# %%
+app: Robyn = Robyn(__file__)
 
 
-@app.get("/")
-def index():
-  return "Hello World!"
+@app.get('/')
+async def index():
+  return 'Hello World!'
 
 
-if __name__ == "__main__":
-  # create a configured "Session" class
-  app.start(host="0.0.0.0", port=8080)
+# %%
+if __name__ == '__main__':
+  app.start(host='127.0.0.1', port=8080)
