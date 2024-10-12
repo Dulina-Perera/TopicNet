1b58873d6c88f2b7e096dfca12421ff5c869b075
Dulina-Perera <dulina.perera@outlook.com>
2024-10-12 21:30:02 +0530

    feat(frontend): support `Markdown` in `Node` component
    design `Node` component using the `tiptap` editor framework

--- frontend/src/lib/components/Node.svelte
+++ frontend/src/lib/components/Node.svelte
@@ -1,78 +1,48 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { Editor } from "@tiptap/core";
+	import { onDestroy, onMount } from "svelte";
+	import { marked } from "marked";
+	import Document from "@tiptap/extension-document";
+	import BulletList from "@tiptap/extension-bullet-list";
+	import ListItem from "@tiptap/extension-list-item";
+	import Heading from "@tiptap/extension-heading";
+	import Paragraph from "@tiptap/extension-paragraph";
+	import Text from "@tiptap/extension-text";
 
-	export let title: string;
 	export let content: string;
 
-	let textareaEl: HTMLTextAreaElement | null = null;
-	let maxHeight: number = 360; // Maximum height (in pixels) before scroll appears
+	let element: HTMLDivElement | undefined;
+	let editor: Editor | undefined;
 
-	// Expand textarea based on content.
-	const autoResize = () => {
-		if (textareaEl) {
-			// Reset height to 'auto' to allow shrinking.
-			textareaEl.style.height = 'auto';
-
-			// Calculate the new height.
-			const newHeight: number = Math.min(textareaEl.scrollHeight, maxHeight);
-			textareaEl.style.height = `${newHeight}px`;
-
-			// Toggle overflow when max height is reached.
-			if (textareaEl.scrollHeight > maxHeight) {
-				textareaEl.style.overflowY = 'auto';
-			} else {
-				textareaEl.style.overflowY = 'hidden';
-			}
-		}
-	};
-
-	// Handle focus and blur for changing border color.
-	const handleFocus = (e: FocusEvent) => {
-		const target = e.target as HTMLTextAreaElement;
-		target.style.borderColor = 'var(--color-primary-700)';
-	};
-
-	const handleBlur = (e: FocusEvent) => {
-		const target = e.target as HTMLTextAreaElement;
-		target.style.borderColor = 'var(--color-secondary-300)';
-		// Reset scroll position to the top
-		target.scrollTop = 0;
-	};
-
-	// Trigger autoResize when the component is mounted to handle initial content.
 	onMount(() => {
-		autoResize();
+		editor = new Editor({
+			element: element,
+			content: marked.parse(content),
+			extensions: [
+				BulletList,
+				Document,
+				Heading.configure({
+					levels: [1, 2, 3]
+				}),
+				ListItem,
+				Paragraph,
+				Text,
+			],
+			injectCSS: false,
+			autofocus: true,
+			editable: true,
+		});
+	});
+
+	onDestroy(() => {
+		if (editor) {
+			editor.destroy();
+		}
 	});
 </script>
 
-<div class="pk bg-white pb-4 pt-4 px-4 w-72" style="border-radius: var(--theme-rounded-container);">
-	<h2 class="text-sm text-center" style="color: var(--color-primary-700);">{title}</h2>
-
-	<textarea
-		bind:this={textareaEl}
-		class="border mt-3 outline-none overflow-hidden p-2 resize-none text-xs w-full"
-		style="
-			border-color: var(--color-secondary-300);
-			border-radius: var(--theme-rounded-container);
-			border-width: var(--theme-border-base);
-			max-height: {maxHeight}px;
-			transition: height 0.2s ease, border-color 0.2s ease;
-		"
-		rows="1"
-		on:input={autoResize}
-		on:focus={handleFocus}
-		on:blur={handleBlur}>{content}</textarea
-	>
-</div>
-
-<style>
-	textarea {
-		scrollbar-color: var(--color-secondary-300) transparent;
-		scrollbar-gutter: stable;
-		scrollbar-width: thin;
-	}
-
-	textarea::-webkit-scrollbar-thumb {
-		border-radius: 10px;
-	}
-</style>
+<div
+	class="bg-white pb-4 pt-4 px-4 w-72"
+	style="border-radius: var(--theme-rounded-container);"
+	bind:this={element}
+/>
--- frontend/src/routes/workspace/+page.svelte
+++ frontend/src/routes/workspace/+page.svelte
@@ -1,7 +1,18 @@
 <script lang="ts">
 	import Board from "$lib/components/Board.svelte";
 	import Node from "$lib/components/Node.svelte";
+
+	let content = `
+# Welcome to TopicNet
+## The interactive way to visualize unstructured text documents
+### Why should you use TopicNet?
+- **Interactive**: Visualize your text data in a way that is easy to understand
+- **Customizable**: Choose from a variety of layouts and themes to suit your needs
+- **Collaborative**: Share your work with others and work together in real-time
+- **Open-source**: Contribute to the project and make it better for everyone
+<br>
+	`
 </script>
 
 <!-- <Board /> -->
-<Node title="TopicNet" content="The interactive way to visualize unstructured text documents" />
+<Node content={content} />
