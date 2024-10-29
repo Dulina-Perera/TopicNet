f95db6aa09f25bc65f9f29699aff75eef8fa448f
IT21353116 <99770954+IT21353116@users.noreply.github.com>
2024-10-30 05:25:03 +0530

    Updated app..ts

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -1,14 +1,20 @@
-import type { PrismaClient } from "@prisma/client";
+import type { PrismaClient } from '@prisma/client';
 
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface Node {
+			id: number;
+			document_id: number;
+			parent_id: number | null;
+			topic_and_content: string;
+		}
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
-	};
+	}
 
 	// eslint-disable-next-line no-var
 	var prismaClient: PrismaClient;
